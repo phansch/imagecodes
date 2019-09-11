849ea3ba7815a31a81a2985819d1c1e7e01befc6
Philipp Hansch <dev@phansch.net>
2019-09-11 07:54:57 +0200

    Fix long data crash
    Fixes #15

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,16 @@
-#![feature(async_await)]
 #![warn(
     rust_2018_idioms,
     future_incompatible
 )]
 
-use qrcode::{QrCode, Version, EcLevel};
+use qrcode::{QrCode, EcLevel};
 use qrcode::render::svg;
 use image::{ColorType, Luma};
 #[cfg(test)]
-use pretty_assertions::{assert_eq};
+use pretty_assertions::assert_eq;
 
 pub fn gen_svg(value: String, size: u32) -> String {
-    let code = QrCode::with_version(value, Version::Normal(3), EcLevel::L).unwrap();
+    let code = QrCode::with_error_correction_level(value, EcLevel::L).unwrap();
 
     code.render()
         .min_dimensions(size, size)
@@ -22,7 +21,7 @@ pub fn gen_svg(value: String, size: u32) -> String {
 }
 
 pub fn gen_jpeg(value: String, size: u32) -> Vec<u8> {
-    let code = QrCode::with_version(value, Version::Normal(3), EcLevel::L).unwrap();
+    let code = QrCode::with_error_correction_level(value, EcLevel::L).unwrap();
     let image = code.render::<Luma<u8>>()
         .min_dimensions(size, size)
         .quiet_zone(false)
@@ -41,7 +40,7 @@ pub fn gen_jpeg(value: String, size: u32) -> Vec<u8> {
 }
 
 pub fn gen_png_buf(value: String, size: u32) -> Vec<u8> {
-    let code = QrCode::with_version(value, Version::Normal(3), EcLevel::L).unwrap();
+    let code = QrCode::with_error_correction_level(value, EcLevel::L).expect("Could not encode QR code");
     let image = code.render::<Luma<u8>>()
         .min_dimensions(size, size)
         .quiet_zone(false)
@@ -62,24 +61,17 @@ pub fn gen_png_buf(value: String, size: u32) -> Vec<u8> {
 
 #[test]
 fn gen_png_buf_test() {
-    let expected = vec![137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 29, 0,
-        0, 0, 29, 8, 0, 0, 0, 0, 115, 248, 56, 211, 0, 0, 0, 230, 73, 68, 65, 84, 120, 156, 93, 207,
-        129, 22, 194, 32, 12, 67, 81, 247, 255, 31, 173, 247, 21, 112, 186, 66, 210, 52, 41, 243, 120,
-        189, 86, 189, 19, 209, 224, 133, 170, 139, 84, 215, 251, 98, 145, 27, 174, 186, 164, 246, 30,
-        89, 52, 238, 157, 178, 124, 103, 227, 153, 202, 233, 3, 115, 46, 229, 213, 249, 210, 129, 195,
-        236, 119, 87, 153, 255, 14, 75, 173, 37, 53, 31, 105, 10, 167, 90, 90, 191, 212, 253, 66, 229,
-        250, 136, 93, 112, 127, 209, 6, 150, 58, 228, 227, 241, 184, 68, 43, 198, 145, 27, 139, 88, 5,
-        214, 26, 173, 221, 216, 51, 54, 204, 134, 105, 163, 55, 94, 233, 45, 112, 244, 104, 3, 57, 162,
-        90, 59, 60, 125, 176, 169, 38, 36, 45, 109, 2, 166, 134, 151, 142, 145, 23, 27, 182, 56, 216,
-        104, 22, 103, 233, 3, 100, 14, 93, 6, 184, 131, 67, 152, 154, 235, 27, 90, 96, 86, 82, 222,
-        148, 231, 147, 0, 191, 25, 210, 85, 122, 40, 120, 181, 14, 37, 99, 118, 180, 100, 55, 248, 191,
-        246, 188, 76, 47, 177, 160, 106, 119, 170, 98, 112, 65, 114, 167, 99, 165, 108, 45, 70, 82,
-        162, 213, 159, 77, 74, 147, 232, 83, 121, 150, 198, 1, 89, 222, 7, 61, 229, 208, 24, 228, 225,
-        188, 29, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130];
+    let expected = vec![137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 21, 0, 0, 0, 21, 8, 0, 0, 0, 0, 140, 124, 250, 74, 0, 0, 0, 141, 73, 68, 65, 84, 120, 156, 77, 205, 1, 22, 2, 49, 12, 2, 209, 246, 254, 135, 94, 63, 160, 251, 36, 53, 133, 73, 182, 222, 51, 61, 231, 62, 247, 56, 21, 167, 127, 81, 70, 194, 185, 9, 54, 75, 49, 149, 36, 244, 30, 74, 75, 122, 41, 20, 182, 132, 122, 168, 57, 255, 161, 19, 58, 201, 45, 150, 54, 164, 230, 191, 96, 197, 101, 13, 107, 116, 82, 179, 118, 250, 169, 232, 210, 59, 138, 11, 116, 51, 104, 154, 128, 133, 42, 41, 6, 15, 78, 249, 68, 155, 24, 7, 193, 189, 48, 197, 135, 107, 233, 121, 33, 198, 22, 201, 128, 221, 26, 244, 55, 74, 126, 169, 110, 139, 98, 94, 90, 7, 247, 199, 155, 241, 53, 26, 151, 141, 105, 190, 3, 225, 3, 10, 41, 110, 16, 248, 95, 17, 190, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130];
     assert_eq!(gen_png_buf("foo".to_string(), 5), expected);
 }
 
 #[test]
 fn gen_svg_test() {
     assert_eq!(gen_svg("f".to_string(), 2), include_str!("../tests/test.svg").trim());
+}
+
+#[test]
+fn long_data_test() {
+    let url = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    gen_png_buf(url.to_string(), 5);
 }
